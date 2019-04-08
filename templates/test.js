1658d87320d8ddbf27d79e83f53ffc352d6630db
cmohri <clara.mohri@gmail.com>
2019-04-07 23:32:25 -0400

    Pie chart updates

--- templates/test.js
+++ templates/test.js
@@ -1,10 +1,13 @@
 function getCsv(schoolname){
     
     d3.csv("https://raw.githubusercontent.com/mzhao3/Poppins/master/data/2015-2016_Demographic_Data_-_Grades_9-12_School.csv", function(csv){
+	//console.log(csv["School Name"]);
 	if (csv["School Name"] == schoolname && csv["Category "] == "All Students"){
-	    console.log(csv);
+	    //console.log(csv);
 	    console.log(csv["School Name"]);
-	    data = [csv["#Asian"], csv["#Black"], csv["#White"], csv["#Hispanic"]];
+	    data = [csv["#Asian"], csv["#Black"], csv["#White"], csv["#Hispanic"], csv["#Other"]];
+	    console.log(data);
+	    var c = ['orange','yellow','blue','green','red']
 
 	    
 	    var svg = d3.select("svg"),
@@ -13,13 +16,14 @@ function getCsv(schoolname){
 		radius = Math.min(width, height) / 2,
 		g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+
 	    svg.append("g")
 		.attr("class", "labels");
-	    
+	    /*
 	    var color = d3.scaleOrdinal()
 		.domain(["Asian", "Black", "White", "Hispanic"])
-		.range(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c', '#792B1A']);
-    
+		.range(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c', '#884aaf']);
+    */
 	    // Generate the pie
 	    var pie = d3.pie();
 	    
@@ -35,17 +39,25 @@ function getCsv(schoolname){
 		.append("g")
 		.attr("class", "arc")
 	    
+	    
+	    
 	    //Draw arc paths
 	    arcs.append("path")
 		.attr("fill", function(d, i) {
-		    return color(i);
+		    console.log(d);
+		    console.log(i);
+		    //console.log(color(i));
+		    return c[i];
 		})
 		.attr("d", arc);
-	    
+
+	  		    
 	    
 	}});
     
     }
 
 getCsv("Stuyvesant High School");
-
+//getCsv("Bronx High School of Science");
+//getCsv("Eleanor Roosevelt High School");
+//getCsv("Benjamin N. Cardozo High School");
